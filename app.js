fafe665e1bb316cd295335cfef1c0de6cb4c1e56
Chaitanya Thogaru <chaituthogaru2006@gmail.com>
2026-06-18 18:09:45 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use((req,res,next)=>{
 app.use(express.urlencoded({extended:true}));
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter)
-app.use("/",userRouter);
+app.use("/listings",userRouter);
 
 
 app.all("*path",(req,res,next)=>{
